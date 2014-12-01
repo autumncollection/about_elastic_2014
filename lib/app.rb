4abff748c5e5953118ba128e4ac9a4cd83d3494f
Tomáš Hrabal <fil.hrabal@gmail.com>
2014-12-01 11:23:43 +0100

    [MOD] Final app

--- lib/app.rb
+++ lib/app.rb
@@ -19,14 +19,40 @@ class BooksSearch < Sinatra::Base
   # book detail
   get '/book/:sysno' do
     # method: get_doc
+    @book = @elastic.get_doc params['sysno']
     slim :detail, :layout => true
   end
 
   post '/search' do
-    # sysno ?
-    #   method: get_docs
-    #   split sysno text with ; as Array
-    #
+    if params.include?('search')
+      if params['search'].match(/\A[\s\d,]+\z/)
+        books  =
+          @elastic.get_docs params['search'].split(',').map { |id| id.strip }
+        @books = books.values
+      else
+        query =
+          { "query" => {
+              "bool" => {
+              }
+            }
+          }
+        if !params.include?('author') && params.include?('title')
+          query['query']['bool']['must'] = { "text" => {"search_title" => params['search']}}
+        elsif !params.include?('title') && params.include?('author')
+          query['query']['bool']['must'] = { "text" => {"author.search_name" => params['search']}}
+        else
+          query['query']['bool']['should'] = [
+            { "text" => {"search_title" => params['search']}},
+            { "text" => {"search_name"  => params['search']}}
+          ]
+        end
+        books = @elastic.search query
+        @books = books.values
+      end
+    end
+
+
+    @books ||= []
     # text ?
     #   method: search
     #   search in search_title AND author.search_name with should
